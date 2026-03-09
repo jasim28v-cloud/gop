bb3c056390e5fe7c8643d5ea57162869222e7781
jasim28v-cloud <jasim28v@gmail.com>
2026-03-09 05:57:11 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import re
 
 def run_news():
-    rss_url = "https://www.aljazeera.net/rss/sport"
+    rss_url = "https://www.skynewsarabia.com/rss/"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
     
     try:
